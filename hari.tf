2a809402e0c66229055725a4ecb14a100e45f4c2
harish Kumar <harishkc007@outlook.com>
2024-02-13 21:58:38 +0530

    Revert "added new tf file"
    This reverts commit fe73d8f01e1f2d68db52b7bf317feff62e716528.

--- hari.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "myBackend" {
-   backend = "azure"
-   config {
-       storage_account_name = "terraform123abc"
-       container_name = "terraform-state"
-       key = "prod.terraform.tfstate"
-   }
-}
